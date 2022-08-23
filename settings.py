a501c6ad77dec7d41145d387d4dcbe2a0d56c2f7
Your Name <jacksontajay920@gmail.com>
2022-08-23 18:35:10 -0400

    Adding players file

---
+++ settings.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.math import Vector2 as vec
+
+#Changing the pacman title
+pygame.display.set_caption('Pac-Thon')
+
+#Changing the pacman icon
+programIcon = pygame.image.load('images/pacman.png')
+pygame.display.set_icon(programIcon)
+
+RealImage = pygame.image.load('images/pacman.png').convert_alpha
+
+# screen settings
+WIDTH, HEIGHT = 610, 670            
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
+
+ROWS = 30
+COLS = 28
+
+# colour settings
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255, 255, 255)
+PLAYER_COLOUR = (190, 194, 15)
+
+# font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
